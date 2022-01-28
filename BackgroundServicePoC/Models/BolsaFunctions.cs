4220f231805d6c35d7ee2deb4caf7484326528cc
Igor Henriques <lycanium@hotmail.com>
2022-01-28 01:24:54 -0300

    Implementado partial class na model

---
+++ BackgroundServicePoC/Models/BolsaFunctions.cs
@@ -0,0 +1,24 @@
+﻿namespace BackgroundServicePoC.Models;
+
+public partial record Bolsa
+{
+    public Bolsa(ulong Id, string Customer)
+    {
+        this.Id = Id;
+        this.Customer = Customer;
+        this.EstimatedTaskDuration = Randomizer.GetRandomUInt();
+    }
+
+    public static List<Bolsa> GenerateList()
+    {
+        return Enumerable
+            .Range(0, 100)
+            .Select(i => new Bolsa((ulong)i, $"Customer Nº {i + 1}"))
+            .ToList();
+    }
+
+    public void SetCustomerName(string name)
+    {
+        this.Customer = name;
+    }
+}
